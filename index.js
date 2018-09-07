f82188f60a034a83e3c7e301f1f7a556f2af383c
Alisa McCormick <muntananuchat@gmail.com>
2018-09-07 00:57:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function nowServing(katzDeliLine) {
   if (katzDeliLine === 0) {
     return `There is nobody waiting to be served!`
   } else {
-    katzDeliLine.shift()
+    var firstPerson = katzDeliLine.shift()
   }
+  return `Now serving ${firstPerson}.`
 }
