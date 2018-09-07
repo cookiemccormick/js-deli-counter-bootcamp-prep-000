f569571cef7a0f0bc5299e2f0002878522c597c0
Alisa McCormick <muntananuchat@gmail.com>
2018-09-07 01:04:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ function takeANumber(line, name) {
   line.push(name)
   return `Welcome, ${name}. You are number ${line.length} in line.`
 }
+
+function nowServing(katzDeliLine) {
+  var firstPerson = katzDeliLine.shift()
+  if (katzDeliLine === 0) {
+    return `There is nobody waiting to be served!`
+  }
+}
